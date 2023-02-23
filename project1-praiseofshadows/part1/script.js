2f30b87f14be59bfedf5cc7f72c622a76f2fa035
ren-jpgs <114003873+ren-jpgs@users.noreply.github.com>
2023-02-23 12:29:08 -0500

    repaste js

--- project1-praiseofshadows/part1/script.js
+++ project1-praiseofshadows/part1/script.js
@@ -5,6 +5,7 @@ let sen2jap = document.querySelector("#sen2jap");
 let eng3 = document.querySelector(".eng3");
 let sen3jap = document.querySelector("#sen3jap");
 
+
 function hover1(){
 	eng.style.opacity ="1";
 }
@@ -16,6 +17,9 @@ function hover2(){
 function hover3(){
 	eng3.style.opacity ="1";
 }
+
+
 sen3jap.addEventListener("mouseover", hover3);
 sen2jap.addEventListener("mouseover", hover2);
 sen1jap.addEventListener("mouseover", hover1);
+
