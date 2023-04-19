fa62e143e9cc81242cb85ea9acd45ba6b4e6157f
ren-jpgs <114003873+ren-jpgs@users.noreply.github.com>
2023-04-19 01:24:50 -0400

    3d demo

--- nyc-data/assets/data.js
+++ nyc-data/assets/data.js
@@ -1,1 +1,18 @@
 const dataUrl='https://data.cityofnewyork.us/resource/3q43-55fe.json'
+
+
+const parseData = (data) => {
+	let colorGray = 0
+
+	// console.log(data)
+}
+
+fetch(dataUrl + '?$limit=5000&city=')
+	.then(response => response.json())
+	.then(data =>{
+		 parseData(data)
+})
+
+rats.forEach(rat => {
+	if (rat.primary_fur_color)
+})
